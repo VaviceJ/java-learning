7c0b354db5345a99b87dfec2db193cc7ee99ef91
VaviceJ <hhbhy@qq.com>
2026-03-08 20:35:27 +0800

    Create ArrayTest.java

---
+++ basic/ArrayTest.java
@@ -0,0 +1,8 @@
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 7, 9};
+        for(int i = 0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
