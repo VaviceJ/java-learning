a160eb84985d8d53f7371d40176f7dbbd0719846
VaviceJ <3578360482@qq.com>
2026-03-08 21:07:24 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Student s = new Student();
+        s.name = "Alice";
+        s.age = 19;
+
+        s.sayHello();
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,9 @@
+public class Student {
+
+    String name;
+    int age;
+
+    void sayHello() {
+        System.out.println("Hi, I'm " + name + ", age " + age);
+    }
+}
